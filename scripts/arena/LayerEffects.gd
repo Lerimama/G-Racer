94340102cd8071e568269a30e45cec81e7feefb4
Lerimama <leriman32@hotmail.com>
2024-02-22 10:55:00 +0100

    Pucanje pred v0.3
    mape

--- scripts/arena/LayerEffects.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-# tukaj so vsi efekti (partikli, lajne ...) ... plejerji, metki in bombe, ... ovire, pickables
-# namenjeno elementom, ki senčke ne morejo producirati drugače kot prek screen šejderja na viewportu
-func _ready() -> void:
-	
-#	Global.effects_creation_layer = self
-	print ("Effects Lejer Z1")
